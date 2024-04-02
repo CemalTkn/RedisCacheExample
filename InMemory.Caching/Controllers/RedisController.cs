2d3567dd42a8c085c8be1cd4d70da8b40ec8958b
CemalTkn <cemaltaskin17@gmail.com>
2024-04-02 16:07:22 +0300

    InMemory Caching

---
+++ InMemory.Caching/Controllers/RedisController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public RedisController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //[HttpGet("set/{name}")]
+        //public void SetName(string name)
+        //{
+        //    _memoryCache.Set("name", name);
+        //}
+
+        //[HttpGet]
+        //public string GetName()
+        //{
+        //    if (_memoryCache.TryGetValue<string>("name",out string name))
+        //    {
+        //        return name.Substring(3);
+        //    }
+        //    return "";
+        //    // return _memoryCache.Get<string>("name");
+        //    // return name.Substring(3);
+        //}
+
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(25),//25 saniye sonra veriler silinecek.
+                SlidingExpiration = TimeSpan.FromSeconds(5)//Eğer 5 saniye boyunca veriler üzerinde işlem yapılmazsa, yine tüm veriler silinecek.
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
